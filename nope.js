2d8a6617243f36dcde2e9eef3cfa34ea2e582f77
Derek <derek@patreon.com>
2016-01-24 14:58:28 -0800

    build

--- nope.js
+++ nope.js
@@ -11,7 +11,7 @@ const fetchKSDetails = (url, callback) => {
     // var url = encodeURIComponent('https://www.kickstarter.com/projects/goodgoodcomedy/good-good-comedy-theatre-philadelphia-pa?ref=home_popular')
     // var url = encodeURIComponent('https://www.kickstarter.com/projects/1587081065/nothing-to-hide-the-documentary')
     // var url = 'https://www.kickstarter.com/projects/657685639/where-is-the-dumpling-emoji'
-    // var url = 'https://www.kickstarter.com/projects/5794927/weed-ramen-and-hemp-ramen-in-chelseany'
+    var url = 'https://www.kickstarter.com/projects/5794927/weed-ramen-and-hemp-ramen-in-chelseany'
     console.log(url)
     const _url = `https://api.import.io/store/connector/d7f013d1-d0d9-4d19-a0be-797ad2cb404b/_query?input=webpage/url:${url}&_apikey=0a3891324b7044bf9fc36b8294051a05388f90666b9ef7b43d8750bbbf5360fcb779954f920498a6d8c14092f4ad87077dd9645299a361bf115df2b108744b5962953a5b70b3828072cae387c1275f6d`
 
@@ -28,8 +28,8 @@ const Suggestions = () => {
 
     return (
         <div>{_suggestions.map((s, i) => {
-            return (<div key={ i }>
-                <strong>{ s.title }</strong>
+            return (<div key={ i } className="suggestion">
+                <input type="checkbox" /> <strong>{ s.title }</strong>
                 <p>{ s.desc }</p>
             </div>)
         })}</div>
@@ -38,23 +38,30 @@ const Suggestions = () => {
 
 const Project = (props) => {
     const message = props.backers <= 0
-        ? <p>0 backers! THANK GOD!!!</p>
+        ? <p className="zerobackers"><strong>0</strong> backers! THANK GOD!!!</p>
         : <p>And help <strong>{ props.backers }</strong> souls save <strong>{ props.goal }</strong> bucks.</p>
 
     return (
         <div>
-            <h1>Stop "{ props.campaign_name }"</h1>
-
-            <img src="https://ksr-ugc.imgix.net/assets/004/987/562/8942877408417c9b816cdadda0d218f1_original.png?v=1448602014&w=680&fit=max&auto=format&lossless=true&s=dc2d1887cc4e77b6c8a6954dcae8f507" />
+            <h1><span className="light-text">Stop:</span> { props.campaign_name }</h1>
 
             { message }
 
-            <Suggestions />
+            <div className='h-flex'>
+                <div className="img-wrapper">
+                    <img className="c-img" src="https://ksr-ugc.imgix.net/assets/004/987/562/8942877408417c9b816cdadda0d218f1_original.png?v=1448602014&w=680&fit=max&auto=format&lossless=true&s=dc2d1887cc4e77b6c8a6954dcae8f507" />
+                </div>
+                <div>
+                    <Suggestions />
+                </div>
+            </div>
+
+
 
             <label>Why should we stop this project?</label>
             <textarea placeholder="Isn't it obvious?"></textarea>
 
-            <button>Stop { props.campaign_name }</button>
+            <button className="mysubmitbtn">Stop { props.campaign_name }</button>
         </div>
     )
 }
@@ -76,7 +83,7 @@ const App = React.createClass({
 
     render() {
         return (
-            <Modal isOpen className="wrapper">
+            <Modal isOpen className="wrapper" style={{ zIndex: 1000 }}>
               { this.state.project ? <Project { ...this.state.project } /> : 'Fetching project…' }
             </Modal>
         )
